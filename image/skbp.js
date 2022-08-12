468d8bc2648e7ac97c85e0b8bb767dca83bfc4ce
yud <phuocduydang010199@gmail.com>
2022-08-12 17:26:12 +0700

    BT_SW_Case.html

---
+++ image/skbp.js
@@ -0,0 +1,36 @@
+function upArrowPressed() {
+    let element = document.getElementById("nobita");
+    element.style.top = parseInt(element.style.top) - 10 + 'px';
+}
+function downArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.top = parseInt(element.style.top) + 10 + 'px';
+}
+function leftArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.left = parseInt(element.style.left) - 10 + 'px';
+}
+function rightArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.left = parseInt(element.style.left) +  + 'px';
+
+}
+function moveSelection(evt) {
+    switch (evt.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
+    }
+}
+function docReady() {
+    window.addEventListener('keydown', moveSelection);
+}
